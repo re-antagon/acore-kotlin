173be234ff6b03fa3db959f07f38ad7cdfee64ff
nonxedy <pos18411@gmail.com>
2026-01-24 12:14:50 +0300

    build: update Kotlin, Gradle, and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,32 @@
 plugins {
-    kotlin("jvm") version "2.1.0"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    kotlin("jvm") version "2.3.0"
 }
 
 group = "org.antagon"
 version = "1.0"
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
 repositories {
     mavenCentral()
-    maven("https://repo.papermc.io/repository/maven-public/") {
-        name = "papermc-repo"
-    }
-    maven("https://oss.sonatype.org/content/groups/public/") {
-        name = "sonatype"
-    }
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://oss.sonatype.org/content/groups/public/")
+    maven("https://jitpack.io")
+    maven("https://mvn.lumine.io/repository/maven-public/")
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-}
-
-val targetJavaVersion = 21
-kotlin {
-    jvmToolchain(targetJavaVersion)
-}
-
-tasks.build {
-    dependsOn("shadowJar")
+    compileOnly("io.papermc.paper:paper-api:1.21.5-R0.1-SNAPSHOT")
+    implementation("com.github.ajneb97:ConditionalEvents:4.65.1")    // Conditional Events api
+    compileOnly("io.lumine:Mythic-Dist:5.9.5")                       // Mythic Mobs api
+    compileOnly("net.luckperms:api:5.4")                             // LuckPerms api
 }
 
 tasks.processResources {
