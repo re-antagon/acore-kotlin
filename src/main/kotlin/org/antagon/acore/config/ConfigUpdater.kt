0c708c6f7ce95da7f3ca6e5a81e0ff4abe584cd3
hgtoiwr <pos18411@gmail.com>
2026-07-10 22:55:14 +0300

    Update config updater

--- src/main/kotlin/org/antagon/acore/config/ConfigUpdater.kt
+++ src/main/kotlin/org/antagon/acore/config/ConfigUpdater.kt
@@ -7,7 +7,7 @@ import java.util.logging.Logger
 
 class ConfigUpdater(private val logger: Logger) {
     companion object {
-        private const val REQUIRED_VERSION = 2
+        private const val REQUIRED_VERSION = 3
     }
 
     fun updateConfiguration(config: FileConfiguration, defaultConfig: FileConfiguration, configFile: File): FileConfiguration {
