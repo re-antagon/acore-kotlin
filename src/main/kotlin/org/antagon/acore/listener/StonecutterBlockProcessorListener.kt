0d5c530bfdc01ff07a05c69e3369fa379011c5c5
PurpleGale <artemy4kin@gmail.com>
2026-07-19 19:18:23 +0300

    Upgrade StonecutterBlockProcessor to track all spawned items using ItemSpawnEvent

--- src/main/kotlin/org/antagon/acore/listener/StonecutterBlockProcessorListener.kt
+++ src/main/kotlin/org/antagon/acore/listener/StonecutterBlockProcessorListener.kt
@@ -10,7 +10,7 @@ import org.bukkit.block.BlockFace
 import org.bukkit.entity.Item
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.player.PlayerDropItemEvent
+import org.bukkit.event.entity.ItemSpawnEvent
 import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.scheduler.BukkitRunnable
@@ -122,8 +122,8 @@ class StonecutterBlockProcessorListener(
      * Adds items to the tracking set if they match any craft input.
      */
     @EventHandler
-    fun onPlayerDropItem(event: PlayerDropItemEvent) {
-        val item = event.itemDrop
+    fun onItemSpawn(event: ItemSpawnEvent) {
+        val item = event.entity
         val type = item.itemStack.type
 
         if (crafts.any { it.matchesMaterial(type) }) {
