ddb3eff3ed44f78a921d23230ba7438d9bb0d836
hgtoiwr <pos18411@gmail.com>
2026-07-22 02:07:46 +0300

    dependency: update packetevents api

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
-    compileOnly("com.github.retrooper:packetevents-spigot:2.7.0")    // PacketEvents API
+    compileOnly("com.github.retrooper:packetevents-spigot:2.13.0")    // PacketEvents API
     compileOnly("com.github.ajneb97:ConditionalEvents:4.65.1")       // Conditional Events api
     compileOnly("io.lumine:Mythic-Dist:5.9.5")                       // Mythic Mobs api
     compileOnly("net.luckperms:api:5.4")                             // LuckPerms api
